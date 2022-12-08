b5b41a381f0e3e3c1147fc91fd58a97ccf01abd8
isakilikya <isakilikya@gmail.com>
2022-12-08 08:22:45 +0300

    Print incoming request to terminal

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from pprint import pprint
+
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root(request: Request):
+    print(f"request:\n{request}\ndict:\n")
+    pprint(request.__dict__)
     return {"message": "Hello World"}
