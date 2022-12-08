4683b207f5830701b56dd09dee6eafd2fbaeb9a3
isakilikya <isakilikya@gmail.com>
2022-12-08 22:45:56 +0300

    Filter incoming requests and create hashed visitorId

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-from pprint import pprint
+import hashlib
 
 from fastapi import FastAPI, Request
+
 
 app = FastAPI()
 
 
+def filter_request_scope_headers(scope_headers: list) -> dict:
+    filtered_request_scope = {}
+    for element in scope_headers:
+        filtered_request_scope[element[0].decode()] = element[1].decode()
+    return filtered_request_scope
+
+
 @app.get("/")
 async def root(request: Request):
-    print(f"request:\n{request}\ndict:\n")
-    pprint(request.__dict__)
-    return {"message": "Hello World"}
+    filtered_request_scope_headers = filter_request_scope_headers(
+        request.scope['headers'].copy()
+    )
+    filtered_request_scope = {
+        'headers': filtered_request_scope_headers,
+    }
+    filtered_request_scope.update({'client_ip': request.scope['client'][0]})
+    string_to_be_hashed = filtered_request_scope['client_ip'] \
+        + filtered_request_scope['headers']['user-agent']
+
+    visitor_id = hashlib.md5(string_to_be_hashed.encode())
+
+    result = {
+        'visitorId': visitor_id.hexdigest(),
+        'filteredRequestScope': filtered_request_scope,
+    }
+    return result
